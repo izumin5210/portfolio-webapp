09538472596e98cff9da78dfef7e847484f9bb0b
izumin5210 <m@izum.in>
2021-06-02 08:14:35 +0000

    Show rendered content

--- src/components/blog/PostRoot.tsx
+++ src/components/blog/PostRoot.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRemark } from "react-remark";
 
 export type Post = {
   title: string;
@@ -7,11 +8,17 @@ export type Post = {
   publishedDate: string;
 };
 
-export const PostRoot: React.VFC<{ post: Post }> = (props) => {
-  return (
-    <dl>
-      <dt>props</dt>
-      <dd>{JSON.stringify(props)}</dd>
-    </dl>
-  );
+export const PostRoot: React.VFC<{ post: Post }> = ({ post }) => {
+  const rendered = useRenderedMarkdown(post.content);
+  return <article>{rendered}</article>;
+};
+
+export const useRenderedMarkdown = (content: string) => {
+  const [reactContent, setMarkdownSource] = useRemark();
+
+  useEffect(() => {
+    setMarkdownSource(content);
+  }, [content, setMarkdownSource]);
+
+  return reactContent;
 };
