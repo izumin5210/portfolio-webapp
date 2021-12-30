36e80db21cd6e1fd9bd726d1e45a1624ae0aebf9
izumin5210 <m@izum.in>
2021-12-30 22:28:34 +0900

    Wrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,23 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useInitRelayEnvironment } from "../lib/RelayEnvironment";
 import { RelayEnvironmentProvider } from "react-relay";
+import { styled } from "@linaria/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const environment = useInitRelayEnvironment(pageProps.initialRecords);
 
   return (
     <RelayEnvironmentProvider environment={environment}>
-      <Component {...pageProps} />
+      <Main>
+        <Component {...pageProps} />
+      </Main>
     </RelayEnvironmentProvider>
   );
 }
+
+const Main = styled.main`
+  margin: 0 auto;
+  max-width: 960px;
+`;
 
 export default MyApp;
