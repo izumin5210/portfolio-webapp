48cabcf449797004bbf1ca00f82c50c918fa2580
izumin5210 <m@izum.in>
2022-01-06 10:15:27 +0900

    Add `article` entry to data

---
+++ _articles/example.md
@@ -0,0 +1,11 @@
+---
+title: Example article
+publishedOn: 2022-01-05
+tags: ['test']
+---
+
+# Example article
+
+- foo
+- bar
+- baz
--- scripts/validate-and-fill-data.mjs
+++ scripts/validate-and-fill-data.mjs
@@ -2,13 +2,19 @@
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-import fs from "fs";
+import { readFileSync } from "fs";
+import fs from "fs/promises";
 import path from "path";
 import yaml from "js-yaml";
 import ogs from "open-graph-scraper";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkExtractFrontmatter from "remark-extract-frontmatter";
+import remarkStringify from "remark-stringify";
 
 const datafile = path.join(process.cwd(), "data.yml");
-const rawData = fs.readFileSync(datafile, "utf-8");
+const rawData = readFileSync(datafile, "utf-8");
 
 /**
  * @typedef Source
@@ -29,7 +35,16 @@ const rawData = fs.readFileSync(datafile, "utf-8");
  */
 
 /**
- * @typedef Entry
+ * @typedef ArticleEntry
+ * @property {string | undefined} title
+ * @property {string} path
+ * @property {string} publishedOn
+ * @property {string[]} tags
+ * @property {boolean} picked
+ */
+
+/**
+ * @typedef ExternalEntry
  * @property {string | undefined} title
  * @property {string} url
  * @property {string} publishedOn
@@ -43,7 +58,7 @@ const rawData = fs.readFileSync(datafile, "utf-8");
  * @property {Tag[]} tags
  * @property {Source[]} sources
  * @property {SourceType[]} sourceTypes
- * @property {Entry[]} entries
+ * @property {(ArticleEntry | ExternalEntry)[]} entries
  */
 
 // prettier-ignore
@@ -62,7 +77,7 @@ for (const tag of data.tags) {
 }
 
 /**
- * @param {Entry} entry
+ * @param {ExternalEntry} entry
  * @return {Source | undefined}
  */
 function inferSource(entry) {
@@ -74,7 +89,7 @@ function inferSource(entry) {
 }
 
 /**
- * @param {Entry} entry
+ * @param {ExternalEntry} entry
  * @return {string | undefined}
  */
 function refineTitle(entry) {
@@ -99,7 +114,7 @@ function refineTitle(entry) {
 }
 
 /**
- * @param {Entry} entry
+ * @param {ExternalEntry|ArticleEntry} entry
  */
 function validateEntry(entry) {
   const warnings = [];
@@ -119,20 +134,49 @@ function validateEntry(entry) {
       }
     }
   }
-  if (entry.source == null) {
-    warnings.push(`failed to determine source`);
-  }
 
-  if (warnings.length > 0) {
-    // eslint-disable-next-line no-console
-    console.warn(`${entry.url}\n${warnings.map((w) => `- ${w}`).join("\n")}\n`);
+  if (!("path" in entry)) {
+    if (entry.source == null) {
+      warnings.push(`failed to determine source`);
+    }
+
+    if (warnings.length > 0) {
+      // eslint-disable-next-line no-console
+      console.warn(`${entry.url}\n${warnings.map((w) => `- ${w}`).join("\n")}\n`);
+    }
   }
 }
 
-void Promise.all(
-  data.entries.map((entry) => {
-    return (
-      ogs({ url: entry.url, timeout: 10 * 1000 })
+/**
+ * @returns {Promise<ArticleEntry[]>}
+ */
+async function loadArticleEntries() {
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkStringify)
+    .use(remarkFrontmatter)
+    .use(remarkExtractFrontmatter, { yaml: yaml.load });
+  const dir = path.join(process.cwd(), "_articles");
+  const filenames = await fs.readdir(dir);
+  return Promise.all(
+    filenames.map(async (filename) => {
+      const body = await fs.readFile(path.join(dir, filename), { encoding: "utf8" });
+      const result = await processor.process(body);
+      return {
+        title: /** @type{string} */ (result.data.title),
+        publishedOn: /** @type{string} */ (result.data.publishedOn),
+        tags: /** @type{string[]} */ (result.data.tags),
+        path: `/articles/${path.basename(filename)}`,
+        picked: false,
+      };
+    })
+  );
+}
+
+void Promise.all([
+  Promise.all(
+    /** @type {ExternalEntry[]} */ (data.entries.filter((entry) => !("path" in entry))).map((entry) => {
+      return ogs({ url: entry.url, timeout: 10 * 1000 })
         .then(({ result }) => {
           if (result.success) {
             return { ...entry, title: result.ogTitle };
@@ -148,22 +192,26 @@ void Promise.all(
         })
         .then((entry) => ({ ...entry, source: inferSource(entry) }))
         .then((entry) => ({ ...entry, title: refineTitle(entry) }))
-        .then((entry) => ({ ...entry, picked: entry.picked ?? false }))
-        // sort tags
-        .then((entry) => ({ ...entry, tags: entry.tags.sort() }))
-        // validate
-        .then((entry) => {
-          validateEntry(entry);
-          return entry;
-        })
-    );
+        .then((entry) => ({ ...entry, picked: entry.picked ?? false }));
+    })
+  ),
+  loadArticleEntries(),
+])
+  .then(([entries1, entries2]) => [...entries1, ...entries2])
+  // sort tags
+  .then((entries) => entries.map((entry) => ({ ...entry, tags: entry.tags.sort() })))
+  // validate
+  .then((entries) => {
+    for (const entry of entries) {
+      validateEntry(entry);
+    }
+    return entries;
   })
-)
   .then((entries) => {
     return entries.sort((e1, e2) => new Date(e2.publishedOn).getTime() - new Date(e1.publishedOn).getTime());
   })
-  .then((entries) => {
-    fs.writeFileSync(
+  .then(async (entries) => {
+    await fs.writeFile(
       datafile,
       yaml.dump({
         ...data,
