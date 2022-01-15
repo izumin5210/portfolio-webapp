46aae7edf59fd388b4058e286be10fd4851f9441
izumin5210 <m@izum.in>
2022-01-15 23:57:14 +0900

    Stop using pseudo-element for link decoration

--- src/lib/ui/TextLink.ts
+++ src/lib/ui/TextLink.ts
@@ -7,22 +7,11 @@ export const textLinkCss = (color: string = colors.blue700) => ({
   "&:focus:not(:focus-visible)": {
     outline: 0,
   },
-  position: "relative",
-  "&:before": {
-    position: "absolute",
-    bottom: "1px",
-    content: '""',
-    width: "100%",
-    transition: "all 300ms",
-    borderBottom: `1px solid ${colorWithOpacity(color, "text.lowEmphasis")}`,
-  },
+  borderBottom: `1px solid ${colorWithOpacity(color, "text.lowEmphasis")}`,
   "&:hover, &:active": {
-    "&:before": {
-      borderBottom: "1px solid transparent",
-    },
+    borderBottom: "1px solid transparent",
   },
-  borderRadius: "4px",
   color,
   textDecoration: "none",
-  transition: "background 300ms",
+  transition: "border 300ms",
 });
