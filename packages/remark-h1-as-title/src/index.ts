865df3a7e1368a7304d2b44c9121dc42c475a33f
izumin5210 <m@izum.in>
2022-01-21 13:40:28 +0900

    Remove h1

--- packages/remark-h1-as-title/src/index.ts
+++ packages/remark-h1-as-title/src/index.ts
@@ -4,12 +4,14 @@ import type { Node, Parent } from "unist";
 
 export const remarkH1AsTitlePlugin: Plugin<[], Parent> = () => {
   return function transformer(tree, file) {
-    for (const node of tree.children) {
+    for (let i = 0; i < tree.children.length; i++) {
+      const node = tree.children[i];
       if (node.type === "yaml") continue;
       if (is(node, "heading") && (node as { depth?: number }).depth === 1) {
         const h1 = node as Parent;
         if (h1.children.length === 1 && h1.children[0].type === "text") {
           file.data.title = (h1.children[0] as Node & { value: string }).value;
+          tree.children = [...tree.children.slice(0, i), ...tree.children.slice(i + 1)];
         } else {
           file.fail(new Error(" h1 at the beginning of the document must be text only"), (node as Node).position);
         }
