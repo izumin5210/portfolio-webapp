192f874ab2a0e0655f5ba1934f40d4553ee3d72c
izumin5210 <m@izum.in>
2022-01-22 00:19:23 +0900

    Move `validate-and-fill-data` to child workspace

---
+++ packages/scripts/bin/update-entries-data.mjs
@@ -0,0 +1,7 @@
+"use strict";
+
+// void require("../lib").updateEntriesData();
+
+import { updateEntriesData } from "../module/index.js";
+
+updateEntriesData();
---
+++ packages/scripts/src/update-entries-data/types.ts
@@ -0,0 +1,39 @@
+export type Source = {
+  name: string;
+  type: string;
+};
+
+export type SourceType = {
+  name: string;
+};
+
+export type Tag = {
+  name: string;
+  displayName?: string;
+};
+
+export type ArticleEntry = {
+  title: string | undefined;
+  path: string;
+  publishedOn: string;
+  source: Source;
+  updatedOn: string;
+  tags: Tag[];
+  picked: boolean;
+};
+
+export type ExternalEntry = {
+  title: string | undefined;
+  url: string;
+  publishedOn: string;
+  source: Source | undefined;
+  tags: Tag[];
+  picked: boolean;
+};
+
+export type Schema = {
+  tags: (Tag & { dependsOn?: string[] })[];
+  sources: (Source & { urlPattern: string })[];
+  sourceTypes: SourceType[];
+  entries: (ArticleEntry | ExternalEntry)[];
+};
