fb58bbd5003fca46f8f498ce9eedd6f854c0ea95
izumin5210 <m@izum.in>
2022-01-25 23:49:22 +0900

    Use `@types/mdast` to implement remark plugins

--- packages/remark-h1-as-title/src/plugin.ts
+++ packages/remark-h1-as-title/src/plugin.ts
@@ -1,38 +1,23 @@
+import type { Heading, Root, Text, YAML } from "mdast";
 import type { Plugin } from "unified";
-import type { Node, Parent } from "unist";
 import { is } from "unist-util-is";
 
-export const remarkH1AsTitle: Plugin<[], Parent> = () => {
+export const remarkH1AsTitle: Plugin<[], Root> = () => {
   return function transformer(tree, file) {
     for (let i = 0; i < tree.children.length; i++) {
       const node = tree.children[i];
-      if (is<YamlNode>(node, "yaml")) continue;
-      if (is<HeadingNode>(node, "heading") && node.depth === 1) {
-        if (node.children.length === 1 && is<TextNode>(node.children[0], "text")) {
+      if (is<YAML>(node, "yaml")) continue;
+      if (is<Heading>(node, "heading") && node.depth === 1) {
+        if (node.children.length === 1 && is<Text>(node.children[0], "text")) {
           file.data.title = node.children[0].value;
           tree.children = [...tree.children.slice(0, i), ...tree.children.slice(i + 1)];
         } else {
-          file.fail(new Error(" h1 at the beginning of the document must be text only"), (node as Node).position);
+          file.fail(new Error(" h1 at the beginning of the document must be text only"), node.position);
         }
         break;
       }
-      file.fail(new Error("the document must start with h1"), (node as Node).position);
+      file.fail(new Error("the document must start with h1"), node.position);
       break;
     }
   };
 };
-
-interface HeadingNode extends Parent {
-  type: "heading";
-  depth: number;
-}
-
-interface TextNode extends Node {
-  type: "text";
-  value: string;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface YamlNode extends Node {
-  type: "yaml";
-}
