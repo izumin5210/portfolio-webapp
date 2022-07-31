21f65ee2f625b069f87a6317a961b32984197b30
izumin5210 <m@izum.in>
2022-07-31 14:04:07 +0900

    Ignore code blocks on meta descriptions

--- packages/remark-extract-lead/src/plugin.ts
+++ packages/remark-extract-lead/src/plugin.ts
@@ -1,23 +1,23 @@
-import type { Heading, Root, YAML } from "mdast";
+import type { Content, Heading, Root, YAML } from "mdast";
 import type { Plugin } from "unified";
 import { is } from "unist-util-is";
 
 export const remarkExtractLead: Plugin<[], Root> = () => {
   return function transformer(tree, file) {
-    let [startIdx, endIdx] = [-1, -1];
+    const leadContents: Content[] = [];
     for (let i = 0; i < tree.children.length; i++) {
       const node = tree.children[i];
-      if (is<YAML>(node, "yaml") && startIdx === -1) continue;
+      if (is<YAML>(node, "yaml") && leadContents.length === 0) continue;
       if (is<Heading>(node, "heading")) {
-        if (node.depth === 1 && startIdx === -1) continue;
+        if (node.depth === 1 && leadContents.length === 0) continue;
         break;
       }
-      if (startIdx === -1) startIdx = i;
-      endIdx = i;
+      leadContents.push(node);
     }
+
     file.data.lead = {
       type: "root",
-      children: tree.children.slice(startIdx, endIdx + 1),
+      children: leadContents,
     };
   };
 };
--- packages/remark-meta-description/src/plugin.ts
+++ packages/remark-meta-description/src/plugin.ts
@@ -1,4 +1,4 @@
-import type { BlockContent, Content, Heading, List, Paragraph, Root, YAML } from "mdast";
+import type { BlockContent, Code, Content, Heading, List, Paragraph, Root, YAML } from "mdast";
 import { toString } from "mdast-util-to-string";
 import type { Plugin } from "unified";
 import { is } from "unist-util-is";
@@ -6,14 +6,15 @@ import { visit } from "unist-util-visit";
 
 export const remarkMetaDescription: Plugin<[], Root> = () => {
   return function transformer(tree, file) {
-    let [startIdx, endIdx] = [-1, -1];
+    const leadContent: Content[] = [];
     for (let i = 0; i < tree.children.length; i++) {
       const node = tree.children[i];
-      if (is<YAML>(node, "yaml") && startIdx === -1) continue;
+      if (is<YAML>(node, "yaml") && leadContent.length === 0) continue;
       if (is<Heading>(node, "heading")) {
-        if (node.depth === 1 && startIdx === -1) continue;
+        if (node.depth === 1 && leadContent.length === 0) continue;
         break;
       }
+      if (is<Code>(node, "code")) continue;
       if (isBlockContent(node) && !is<List>(node, "list") && !is<Paragraph>(node, "paragraph")) {
         file.fail(new Error(`meta description cannot has ${node.type} content`), node.position);
       }
@@ -21,22 +22,20 @@ export const remarkMetaDescription: Plugin<[], Root> = () => {
         file.fail(new Error(`meta description cannot has ${bc.type} content in ${node.type}`), bc.position);
       }
 
-      if (startIdx === -1) startIdx = i;
-      endIdx = i;
+      leadContent.push(node);
     }
 
-    const descMdNodes = tree.children.slice(startIdx, endIdx + 1);
-    for (let i = 0; i < descMdNodes.length; i++) {
-      const node = descMdNodes[i];
+    for (let i = 0; i < leadContent.length; i++) {
+      const node = leadContent[i];
       if (is<List>(node, "list")) {
-        descMdNodes[i] = {
+        leadContent[i] = {
           type: "paragraph",
           children: node.children.map((li) => ({ type: "text", value: `· ${toString(li)}; ` })),
         };
       }
     }
 
-    file.data.metaDescription = toString({ type: "root", children: descMdNodes }).replace(/\n/g, " ");
+    file.data.metaDescription = toString({ type: "root", children: leadContent }).replace(/\n/g, " ");
   };
 };
 
