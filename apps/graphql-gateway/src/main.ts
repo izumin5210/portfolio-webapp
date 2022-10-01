e1df0018dac4d4b45770802a5356e6c019e3531c
izumin5210 <m@izum.in>
2022-10-01 14:20:49 +0900

    Setup graphql-gateway skeleton

---
+++ apps/graphql-gateway/src/main.ts
@@ -0,0 +1,10 @@
+import { createServer } from "@graphql-yoga/node";
+import { schema } from "./schema";
+
+async function main() {
+  const server = createServer({ schema });
+
+  await server.start();
+}
+
+void main();
