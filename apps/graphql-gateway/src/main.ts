f75595511f5acad7d852586ccb91c026dd6c258f
izumin5210 <m@izum.in>
2022-10-01 18:54:06 +0900

    Configure PORT

--- apps/graphql-gateway/src/main.ts
+++ apps/graphql-gateway/src/main.ts
@@ -3,7 +3,10 @@ import { createContext } from "./context";
 import { schema } from "./schema";
 
 async function bootstrap() {
-  const server = createServer({ schema, context: createContext });
+  const port = Number(process.env.PORT);
+  if (isNaN(port)) throw new Error("require PORT");
+
+  const server = createServer({ schema, context: createContext, port });
 
   await server.start();
 }
